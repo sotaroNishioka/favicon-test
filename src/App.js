a38f48d71ded7ddde2bff78474c453dc1b0800b0
sotaroNishioka <nishiokanandesuka@gmail.com>
2023-09-06 19:07:10 +0900

    Merge branch

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ function App() {
   return (
     <div className="App">
       <Helmet>
+        <meta
+          name="description"
+          content={`${random}${random}${random}${random}${random}${random}${random}${random}`}
+        />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
@@ -27,6 +32,22 @@ function App() {
             </svg>
           `}
         />
+        <link
+          rel="apple-touch-icon"
+          href={`
+            data:image/svg+xml;
+            utf8,
+            <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+              <text font-family="'Dancing Script', cursive" font-size="16" x="50%" y="50%" dominant-baseline="middle" text-anchor="middle">
+                ${random}
+              </text>
+            </svg>
+          `}
+        />
+        <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+        <title>
+          ${random}${random}${random}${random}${random}
+        </title>
       </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
